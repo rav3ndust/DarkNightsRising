f5f85450aad72e796d5f011a444d87ea1fb713a3
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2021-05-17 17:43:20 -0500

    Add files via upload
    add introduction and "first scene" scripts. 
    
    intro script creates the character and generates the variables.txt file, first scene is first part of the game

---
+++ first-scene.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+source variables.txt	# link the variables.txt file in so we can call the variables we made earlier. 
+
+# This is the First Scene of the game. 
+# In this scene, the character will learn about the story and how they can begin progressing through it. 
+# We will begin building out various choices the character can make in this game. 
+#
+# Remember the variables: 
+# NAME is the player's name. CHARNAME is the characters name. RACE is the race of the character, and PLACE is the kingdom the character is from. 
+#
+# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+espeak "Let us get started." & dialog --msgbox "Let us get started." 100 50 
+
+# The next part of the story will involve the player learning about their location
+espeak "$CHARNAME wakes up in their home, feeling isolated and lethargic." & dialog --msgbox "$CHARNAME wakes up in their home, feeling isolated and lethargic." 100 50 
+
+espeak "The area around you begins to swirl, almost as if you are in a haze." & dialog --msgbox "The area around you begins to swirl, almost as if you were in a haze." 100 50 
+
+espeak "$CHARNAME wakes up in front of a mirror, with a sinister looking reflection staring back at them." & dialog --msgbox "$CHARNAME wakes up in front of a mirror, with a sinister-looking reflection staring back at them." 100 50  
+
+espeak "You feel a sense of dread as you look into the mirror. What do you do?" & dialog --msgbox "You feel a sense of dread as you look into the mirror. What do you do?" 100 50 
+
+espeak "Would you like to look into the mirror, or back away? Press enter to continue." & dialog --msgbox "Would you like to look into the mirror, or back away? Press ENTER to continue." 100 50 
+# present input allowing player to decide, execute an if/then condition based on that condition
+read -p "Please enter LOOK or BACK AWAY to decide: " LOOKORBACK
+
+	if [[ $LOOKORBACK = 'LOOK' ]]
+		then espeak "You stare into the mirror, and see a demonic face looking back at you." & dialog --msgbox "You stare into the mirror, and see a demonic face looking back at you." 100 50 
+		     espeak "The demon says the following. Hello, $CHARNAME of $PLACE. I know your heart, and I know your spirit. You are not the only one of the $RACE kind feeling this way. Together, we can help you realize your true potential." & dialog --msgbox "The demon says the following: Hello, $CHARNAME of $PLACE. I know your heart, and I know your spirit. You are not the only one of the $RACE kind feeling this way. Together, we can help you realize your true potential.'" 100 50
+		     espeak "You feel terrified. You don't know what to do next, only that your heart feels like it is sinking into your chest." & dialog --msgbox "You feel terrified. You don't know what to do next, only that your heart feels like it is sinking into your chest." 100 50 
+             espeak "You hear the sound of a gust of wind as you are seemingly pulled into the mirror." & dialog --msgbox "You hear the sound of a gust of wind as you are seemingly pulled into the mirror." 100 50 
+             espeak "It is time to explore your inner demons, $NAME. Press enter to continue." & dialog --msgbox "It is time to explore your inner demons, $NAME. Press ENTER to continue." 100 50 
+             exit && ./second-scene.sh
+        else espeak "You begin to back away slowly, only to hear a huge howl." & dialog --msgbox "You begin to back away slowly, only to hear a huge howl." 100 50
+             espeak "You wonder what it could have been, only to see a demonic face appearing in the mirror." & dialog --msgbox "You wonder what it could have been, only to see a demonic face appearing in the mirror." 100 50
+             espeak "Out of the blue, you hear an evil voice shout the following. Hello, $CHARNAME of $PLACE. I know your heart, and I know your spirit. You are not the only one of the $RACE kind feeling this way. Together, we can help you realize your true potential." & dialog --msgbox "Out of the blue, you hear an evil voice shout the following: 'Hello, $CHARNAME of $PLACE. I know your heart, and I know your spirit. You are not the only one of the $RACE kind feeling this way. Together, we can help you realize your true potential.'" 100 50 
+             espeak "Suddenly, a gust of wind grabs you out of nowhere, and you are forced headfirst into the mirror, whether you like it or not." & dialog --msgbox "Suddenly, a gust of wind grabs you out of nowhere, and you are forced headfirst into the mirror, whether you like it or not." 100 50
+             espeak "It is time to explore your inner demons, $NAME. Press enter to continue." & dialog --msgbox "It is time to explore your inner demons, $NAME. Press ENTER to continue." 100 50 
+             exit && ./second-scene.sh
+        fi
+exit
---
+++ intro.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#
+# Dark Night Rising by rav3ndust
+#
+# A text-based adventure game guided by a robot. 
+#
+# this aims to be a simple adventure game that is written in nothing but shell script.
+#
+# a very odd way to write a game to be sure, but I am bored and need a side project. 
+#
+# someone help me make this awesome. it could even be fun to play on Linux phones. 
+#
+# ...maybe.
+#
+# This is the introduction and character-creation stage of this game.
+#
+# global variables for this game will be: 
+# NAME: the name of the player
+# CHARNAME: the player's desired character name
+# RACE: the player's desired character race
+# PLACE: the player's desired fictional hometown
+#
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+#
+# Present the player with the introduction. 
+espeak "Hello. Welcome to Dark Night Rising. This is a game about finding yourself, but keep in mind that the journey is not about the destination, but the path it takes to get there." & dialog --msgbox "Hello. Welcome to Dark Night Rising. This is a game about finding yourself, but keep in mind that the journey is not about the destination, but the path it takes to get there." 100 50  
+
+# Next, we want the player to introduce themselves to us, and select what kind of experience they would like to have. 
+espeak "I am Raven, your guide through this game. Please tell me a little bit about yourself." & dialog --msgbox "I am Raven, your guide through this game. Please tell me a little bit about yourself." 100 50 
+#
+# start with NAME. the script should read the user's input as their name, and the variable can be called using NAME from now on.
+# 
+# we are using a *read* command instead of an inputbox so that we can retain the input as a variable, instead of a temporary piece of data. 
+#
+# we will then ask the player what they would like to name their character, what race their character is, and to come up with a location name. 
+#
+# we will store all of these values as variables using the read command, allowing us to call them again later in dialog boxes.
+#
+espeak "So, wanderer. What is your name? Press enter to continue." & dialog --msgbox "So, wanderer - what is your name? Press ENTER to continue." 100 50  
+read -p "Please enter your name: " NAME
+
+# accept and verify NAME, move onto the next step 
+espeak "It is nice to meet you, $NAME. Now, let us learn a little more about you." &  dialog --msgbox "It is nice to meet you, $NAME. Now, let us learn a little bit more about you." 100 50 
+
+espeak "So, tell me, $NAME, what would you like to call your character? Press enter to continue." & dialog --msgbox "So, tell me, $NAME, what would you like to call your character? Press ENTER to continue." 100 50 
+
+# take the input for the character name. it will be stored as a variable called CHARNAME. 
+read -p "Please enter a character name now: " CHARNAME 
+
+# accept the input, move onto the next
+espeak "Excellent. What race will $CHARNAME hail from? Be creative. Press enter to continue." & dialog --msgbox "Excellent. What race will $CHARNAME hail from? Be creative. Press ENTER to continue." 100 50
+
+# take input for the race. it will be stored as a variable called RACE. 
+read -p "Please enter your desired race for $CHARNAME: " RACE
+
+# verify the input, move on to the next
+espeak "Cool, so $CHARNAME is from the $RACE race." & dialog --msgbox "Cool, so $CHARNAME is from the $RACE race." 100 50  
+espeak "Next, let us decide where $CHARNAME hails from. What should their home kingdom be called? Press enter to continue." & dialog --msgbox "Next, let us decide where $CHARNAME hails from. What should their home kingdom be called? Press ENTER to continue." 100 50 
+
+# take input for the location. the data will be stored as a variable called PLACE
+read -p "Where does $CHARNAME come from? Come up with a creative name for a location: " PLACE
+
+# take all of the desired input, verify it. if the player is happy, move onto Scene 1 of the game by invoking scene1.sh. if not, exit so the player can restart the game. 
+espeak "So, $NAME. We have decided that your character is named $CHARNAME, is of the $RACE race, and comes from the kingdom of $PLACE." & dialog --msgbox "So, $NAME. We have decided that your character is named $CHARNAME, is of the $RACE race, and comes from the kingdom of $PLACE." 100 50 
+
+# ask the user if they are happy with these choices using an if-then condition
+espeak "Are you happy with this character, $NAME? Type yes or no on the next screen to decide. Press enter to continue." & dialog --msgbox "Are you happy with this character, $NAME? Type YES or NO on the next screen to decide. Press ENTER to continue." 100 50 
+
+read -p "Are you happy with this configuration? Please enter YES or NO: " ANSWER
+
+	if [[ $ANSWER = "YES" ]]
+		then touch variables.txt	# create a textfile to call variables from other scripts, append all of them 
+		echo "NAME='$NAME'" >> variables.txt  # append the NAME variable to the textfile
+		echo "CHARNAME='$CHARNAME'" >> variables.txt # append the CHARNAME variable to the textfile
+		echo "RACE='$RACE'" >> variables.txt	  # append the RACE variable to the textfile
+		echo "PLACE='$PLACE'" >> variables.txt   # append the PLACE variable to the textfile
+		./first-scene.sh		# launch into the first scene of the game
+	else espeak "Oh no! Let us start again until you get it right." & dialog --msgbox "Oh no! Let us start again until you get it right." 100 50 && exit && ./scene-1.sh
+	# if an answer other than YES is given, the script will exit and reload, giving the player the chance to begin their character creation process again.
+	fi
+exit 
