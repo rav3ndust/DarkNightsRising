88afb7f1a863da12eb788ed1ac87bbfd4a07fbed
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2021-07-01 03:09:53 -0500

    added some random junk :D
    -added testani.sh for the test animation, have no clue where i'll use it yet. maybe i will scrap it. 
    -I also added the test "soundtrack" files. will give them a spin and see if everything works well.

---
+++ animations/testani.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo -ne 'Loneliness#####.\r'
+sleep 3
+echo -ne 'Isolation###########.\r'
+sleep 3
+echo -ne 'Sadness#######################.\r'
+sleep 3
+echo -ne 'Depression###############################.\r'
+sleep 3
+clear
+echo -ne '. 													\r'
+sleep 3
+echo -ne '. . 														\r'
+sleep 3
+echo -ne '. . . 															\r'
+sleep 3
+clear
+echo -ne 'What continues to haunt me#############################. 						\r'
+sleep 3
+clear
