c6c091f04b3c91cd7e2aa42a85a45e4ca75c34e8
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2021-07-07 16:40:55 -0500

    Update install.sh
    update for the installation of new Scenes and dependencies. 
    We also added new comments to make things more clear on how the dependencies are used. we need to update this in the README.md as well.

--- install.sh
+++ install.sh
@@ -8,17 +8,28 @@
 #
 # at the moment, the game assumes a Debian/Ubuntu-based distribution
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
+notify-send "DarkNightsRising Installer" "Currently installing Dark Nights Rising. Hold tight..."
 echo "Installing Dark Nights Rising..."
 
 # install the dependencies for the games
 #
-# the required deps are flite and dialog
-
-sudo apt install flite dialog -y
+# the required deps are flite, mpg123 and dialog
+#
+# - flite is used for vocal synthesis
+# - mpg123 is used to control the game's soundtrack 
+# - dialog is used to present the game's GUI elements
+#
+# more deps may be required as development goes on, but I am striving to keep things as minimal as possible. 
+sudo apt install flite dialog mpg123 -y
 
 # make each script (Game Scene) executable
-sudo chmod +x intro.sh && sudo chmod +x first-scene.sh && sudo chmod +x second-scene.sh && chmod +x third-scene.sh
+chmod +x deathScript.sh
+chmod +x intro.sh 
+chmod +x first-scene.sh
+chmod +x second-scene.sh
+chmod +x third-scene.sh
+chmod +x fourth-scene.sh
+chmod +x fifth-scene.sh
 
 # make the game invokeable systemwide. 
 #
@@ -27,7 +38,7 @@ sudo chmod +x intro.sh && sudo chmod +x first-scene.sh && sudo chmod +x second-s
 sudo cp intro.sh /usr/bin/darknightsrising
 
 # notify the user that the process is complete
-
+notify-send "DarkNightsRising Installer" "Thanks for installing Dark Nights Rising! Enjoy."
 echo "Dark Nights Rising is installed. Please start it by executing the intro.sh script or typing darknightsrising from anywhere on your GNU/Linux system."
 echo "Enjoy the game! If you need any help, please see https://github.com/rav3ndust/DarkNightsRising to file bugs or learn more."
 
