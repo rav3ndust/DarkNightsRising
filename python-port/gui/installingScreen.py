88ce1014e3334d3c06ee1fe0a8f1f8bb2004143b
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2021-10-11 06:29:26 -0500

    Update installingScreen.py
    update the installerScreen

--- python-port/gui/installingScreen.py
+++ python-port/gui/installingScreen.py
@@ -1,4 +1,10 @@
-# installer screen? 
+'''
+This is the installation window that shows a handy progress bar alerting the user that the game is
+installing.
+
+When this is ran, the game's scripts are marked as executable by the system, and the dependences that the
+game relies on are installed if they don't already exist on the system.
+'''
 import PySimpleGUI as gui
 import os
 # send a notification to let the user know the game is being installed.
