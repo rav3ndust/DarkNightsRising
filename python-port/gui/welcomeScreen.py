3867512a28afb4d24b87b0d60dc85cdc5704f20f
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2021-10-11 03:25:01 -0500

    Add files via upload
    added the first welcome screen for the new python port 
    
     i will be working on this on the side to make it even better

---
+++ python-port/gui/welcomeScreen.py
@@ -0,0 +1,30 @@
+'''
+This is the beginning of the Python port of DarkNightsRising. 
+
+We are going to try to make it into a Python package so it can be easily portable across systems. 
+
+This is the first welcome screen
+'''
+#initial Welcome Window for DarkNightsRising
+import PySimpleGUI as gui
+
+gui.theme('Black')  #theming
+
+# add the stuff inside the window
+windowLayout = [[gui.Text("Welcome to Dark Nights Rising!")],
+                [gui.Text("Can you handle your inner demons?")],
+                [gui.Text("Select an option below to get started.")],
+                [gui.Button('Play'), gui.Button('Load Previous Game')],
+                [gui.Button('Delete Previous Game'), gui.Button('Exit the Game')]]   
+
+# spawn the window
+welcomeWindow = gui.Window('Dark Nights Rising', windowLayout)
+
+# loop for processing events and taking inputs 
+while True:
+    event, values = welcomeWindow.read()
+    if event == gui.WIN_CLOSED or event == 'Exit the Game':
+        break
+    print('Exiting the game!')
+
+welcomeWindow.close()
