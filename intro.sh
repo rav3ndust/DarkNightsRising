2586e91faf3ba631c84ea695f1e22ab862cad120
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2024-03-26 15:58:51 -0500

    Update intro.sh
    fixed a bug where playerdata wasn't properly being saved as variables to write to playerconfig

--- intro.sh
+++ intro.sh
@@ -15,13 +15,8 @@ EOF
 line1="Hello. Welcome to Dark Nights Rising. This is a game about finding yourself, but keep in mind that the journey is not about the destination, but the path it takes to get there."
 line2="I am Raven, your guide through this game. Please tell me a little about yourself."
 line3="So, wanderer. What is your name? Please enter it to continue."
-line4="It is nice to meet you, $NAME. Now let us learn a little more about you."
-line5="So, tell me, $NAME, what would you like to call your character? Please enter it to continue."
-line6="Excellent. What race will $CHARNAME hail from? Be creative. Please enter it to continue."
-line7="Cool, so $CHARNAME is from the $RACE race."
-line8="Next, let us decide where $CHARNAME hails from. What should their home kingdom be called? Please enter it to continue."	
-line9="So, $NAME. We have decided that your character is named $CHARNAME, is of the $RACE race, and comes from the kingdom of $PLACE."
-line10="Are you happy with this character, $NAME? Type 'yes' or 'no' to continue."	
+# we have to split the other lines below, to make them available for variable declaration provided by the player
+# - - - - - - - - - - - - - - - - - - 
 # functions
 say () {
 	# uses flite to speak to the user
@@ -48,17 +43,25 @@ main () {
 	clear
 	execute_track
 	printf '%s\n' "${title_text}"; sleep 3; small_loader_progression
-	printf '%s\n' "${line1}" & say "${line1}"; sleep 1
-	printf '%s\n' "${line2}" & say "${line2}"; sleep 1
-	printf '%s\n' "${line3}" & say "${line3}"; read -p "Please enter your name: " NAME
-	printf '%s\n' "${line4}" & say "${line4}"; sleep 1
-	printf '%s\n' "${line5}" & say "${line5}"; read -p "Please enter your character's name: " CHARNAME
-	printf '%s\n' "${line6}" & say "${line6}"; read -p "Please enter desired character race: " RACE
-	printf '%s\n' "${line7}" & say "${line7}"; sleep 1
-	printf '%s\n' "${line8}" & say "${line8}"; read -p "Please enter desired character's home: " PLACE
-	printf '%s\n' "${line9}" & say "${line9}"; sleep 1
-	printf '%s\n' "$line10" & say "${line10}"; read -p "Are you happy with this configuration? " CONFIG_FINAL
-	if [[ $CONFIG_FINAL = "yes" ]]; then
+	echo "${line1}" & say "${line1}"; sleep 1
+	echo "${line2}" & say "${line2}"; sleep 1
+	echo "${line3}" & say "${line3}"; read -p "Please enter your name: " NAME
+	line4="It is nice to meet you, $NAME. Now let us learn a little more about you."
+	line5="So, tell me, $NAME, what would you like to call your character? Please enter it to continue."
+	# - - - - - - - - - - - - - - - - - - 2
+	echo "${line4}" & say "${line4}"; sleep 1
+	echo "${line5}" & say "${line5}"; read -p "Please enter your character's name: " CHARNAME
+	line6="Excellent. What race will $CHARNAME hail from? Be creative. Please enter it to continue."
+	echo "${line6}" & say "${line6}"; read -p "Please enter desired character race: " RACE
+	line7="Cool, so $CHARNAME is from the $RACE race."
+	echo "${line7}" & say "${line7}"; sleep 1
+	line8="Next, let us decide where $CHARNAME hails from. What should their home kingdom be called? Please enter it to continue."	
+	echo "${line8}" & say "${line8}"; read -p "Please enter desired character's home: " PLACE
+	line9="So, $NAME. We have decided that your character is named $CHARNAME, is of the $RACE race, and comes from the kingdom of $PLACE."
+	echo "${line9}" & say "${line9}"; sleep 1
+	line10="Are you happy with this character, $NAME? Type 'yes' or 'no' to continue."
+	echo "$line10" & say "${line10}"; read -p "Are you happy with this configuration? " PLAYERCONFIG_FINALISE
+	if [[ $PLAYERCONFIG_FINALISE = "yes" ]]; then
 		# save the player config in a file for reference by other files
 		local playerconfig="playerconfig.txt"
 		touch $playerconfig
@@ -73,8 +76,8 @@ main () {
 		printf '%s\n' "${oops}" & say "${oops}"; sleep 1; bash intro.sh
 	fi
 }
-# - - entry
+# - - - entry 
 main
-# TODO: 
+# NOTE: 
 #	- this is a new testing version of DNR to clean up its codebase.
 #	- we are working to organize the code and keep it as maintainable as possible for our small team working on it.
